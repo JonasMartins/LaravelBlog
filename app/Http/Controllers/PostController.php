17ce7a8dbbc81a427079084900d96c5c17655b95
JonasMartins <jonas.s.martins@hotmail.com>
2017-02-28 17:21:58 -0300

    Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index(){
-        $posts = Post::all();
+        $posts = Post::orderBy('id','desc')->paginate(5);
         return view('posts.index')->withPosts($posts);
     }
 

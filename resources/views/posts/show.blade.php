5d68a899c10f0e7ce7264d31926b4513fc186253
JonasMartins <jonas.s.martins@hotmail.com>
2017-03-01 19:16:51 -0300

    Slug routes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,6 @@
   <div class="row">
     <div class="col-md-8">
       <h1>{{ $post->title }}</h1>
-      
       <p class="lead">{{ $post->body }}</p>
     </div>
 
@@ -12,7 +11,7 @@
       <div class="well">
         <dl class="dl-horizontal">
           <label>Url Slug:</label>
-          <p><a href="{{ url($post->slug) }}">{{ url($post->slug) }}</a></p>
+          <p><a href="{{ url('blog/'.$post->slug) }}">{{ url('blog/'.$post->slug) }}</a></p>
         </dl>
         <dl class="dl-horizontal">
           <label>Created At:</label>

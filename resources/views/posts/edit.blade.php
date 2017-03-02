754e365b21cfacc879113746c32fe063bd658ef0
JonasMartins <jonas.s.martins@hotmail.com>
2017-03-02 11:19:56 -0300

    Resolvendo bug update unique Slug

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -12,8 +12,8 @@
           <textarea type="text" class="form-control input-lg" id="title" name="title" rows="1" style="resize:none;">{{ $post->title }}</textarea>
         </div>
         <div class="form-group">
-          <label for="title">Slug:</label>
-          <textarea type="text" class="form-control input-lg" id="title" name="title" rows="1" style="resize:none;">{{ $post->slug }}</textarea>
+          <label for="slug">Slug:</label>
+          <textarea type="text" class="form-control input-lg" id="slug" name="slug" rows="1" style="resize:none;">{{ $post->slug }}</textarea>
         </div>
         <div class="form-group">
           <label for="body">Body:</label>
